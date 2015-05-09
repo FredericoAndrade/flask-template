6983f34745aeb477cd86136d32e33874dfda7e33
Fredward <faandrade@gmail.com>
2015-05-09 15:21:59 -0400

    hello

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 #http://blog.y3xz.com/blog/2012/08/16/flask-and-postgresql-on-heroku
+#hello
 
 class Logger(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
